0d9355db48381450bf5f02c407d003d0e25f1da4
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2022-09-07 23:05:09 +0530

    Update Mainclass.java

--- Mainclass.java
+++ Mainclass.java
@@ -1,12 +1,13 @@
-public class Swapping2Numbers {
+ public class Swapping2Numbers {
     public static void main ( String [ ] args ) {
         int a = 10 , b = 20 ;
-        System.out.println ( " Before swapping values are .. " + a + " " + b ) ;
+        System.out.println("Before swapping values are .. " + a + "  " + b) ;
         // Logic1 Third variable
         int t = a ;
         a = b ;
         b = t ;
        
-}
-    }
-        System.out.println ( " After swapping values are .. " + a + " " + b ) ;
+
+        System.out.println("After swapping values are .. " + a + "  " + b) ;
+ }
+ }
