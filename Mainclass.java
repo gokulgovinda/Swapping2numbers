ca2818d2d6ead8763eaf09dd5a7b676bb5160b91
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2022-09-07 23:01:02 +0530

    Create Mainclass.java

---
+++ Mainclass.java
@@ -0,0 +1,12 @@
+public class Swapping2Numbers {
+    public static void main ( String [ ] args ) {
+        int a = 10 , b = 20 ;
+        System.out.println ( " Before swapping values are .. " + a + " " + b ) ;
+        // Logic1 Third variable
+        int t = a ;
+        a = b ;
+        b = t ;
+       
+}
+    }
+        System.out.println ( " After swapping values are .. " + a + " " + b ) ;
