5a3981f40da8e4367b854b713b30f93494b9e1d4
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2022-09-08 21:38:17 +0530

    Update Mainclass.java

--- Mainclass.java
+++ Mainclass.java
@@ -2,11 +2,16 @@
     public static void main ( String [ ] args ) {
         int a = 10 , b = 20 ;
         System.out.println("Before swapping values are .. " + a + "  " + b) ;
-        // Logic1 Third variable
+        // Logic1  using Third variable
         int t = a ;
         a = b ;
         b = t ;
        
+//      without using third variable
+//      a=a^b;
+//      b=a^b;
+//      a=a^b;
+
 
         System.out.println("After swapping values are .. " + a + "  " + b) ;
  }
